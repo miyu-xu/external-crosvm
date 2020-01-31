da39d15e08e7d0f2ecc41298fb1ec33184b86cee
Lingfeng Yang <lfy@google.com>
2020-01-31 13:55:35 -0800

    to be upstreamed: Allow registering memory from current address space
    bug: 146066070
    bug: 153580313
    
    This is good for enabling non-exportable Vulkan host coherent memory,
    along with anything else where an exportable OS object isn't supported.
    
    This CL introduces two structures:
    
    1. NonMappedDeviceMemory, which represents an existing buffer in the
    current address space for purposes of sharing device memory to the guest
    in the case where the device memory is not compatible with the mmpa
    interface. This is common in Vulkan when VkDeviceMemory is host visible
    but not external, or external but based on an opaque fd.
    
    We make the assumption that the actual host address range backing the
    region outlives NonMappedDeviceMemory.
    
    The intended use of NonMappedDeviceMemory is for it to be inherited as
    the base of another Trait object, which will specify any additional
    cleanup when it's dropped from the hypervisor's memory tables, and can
    override how to obtain the host pointer and size.
    
    2. Usually, we would send such memory requests over a socket to the main
    thread. However, since these new objects require more metadata than
    other requests that are sent over the wire (because there's information
    about inheritance and refcounts), we also introduce
    NonSocketExpressibleRequests, which communicates a single request at a
    time to the main thread but can handle passing over first-class Rust
    objects.
    
    We then initialize a single instance on boot and pass an Arc<Mutex>
    reference to it for users, starting with virtio gpu.
    
    Note that this NonSocketExpressibleRequests will not work in the sandbox
    case. In the sandbox case, we will then have to figure out how to
    serialize/deserialize NonMappedDeviceMemory requests over a socket.
    
    The workflow is demonstrated in the gfxstream implementation for
    non-exportable Vk memory:
    
    1. Guest triggers vkMapMemory on host
    2. Host generates a hostmem id, sends back to guest
    3. Guest asks to RESOURCE_CREATE_V2 with the hostmem id
    4. Guest asks to RESOURCE_MAP with the resulting resource id
    5. Host gets hva, size from the backend using the resource id
    6. Host maps hva, size via KVM to the guest pci offset
    7. Guest maps that PCI offset, exposing memory to guest userspace
    
    Change-Id: I3b099b308aec45a313a8278ed6274f9dec66c30b

--- vm_control/src/lib.rs
+++ vm_control/src/lib.rs
@@ -15,13 +15,17 @@ use std::fs::File;
 use std::io::{Seek, SeekFrom};
 use std::mem::ManuallyDrop;
 use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
+use std::sync::Arc;
 
 use libc::{EINVAL, EIO, ENODEV};
 
 use kvm::{IrqRoute, IrqSource, Vm};
 use msg_socket::{MsgOnSocket, MsgReceiver, MsgResult, MsgSender, MsgSocket};
-use resources::{Alloc, GpuMemoryDesc, MmioType, SystemAllocator};
-use sys_util::{error, Error as SysError, EventFd, GuestAddress, MemoryMapping, MmapError, Result};
+use resources::{Alloc, GpuMemoryDesc, MmioType, NonMappedDeviceMemory, SystemAllocator};
+use sync::Mutex;
+use sys_util::{
+    error, Error as SysError, EventFd, GuestAddress, MemoryMapping, MmapError, Result,
+};
 
 /// A file descriptor either borrowed or owned by this.
 #[derive(Debug)]
@@ -251,6 +255,11 @@ impl Display for UsbControlResult {
     }
 }
 
+/// Not everything can be completely communicated over MsgOnSocket
+pub struct NonSocketExpressibleRequests {
+    pub non_mapped_memory_requests: Vec<NonMappedDeviceMemory>,
+}
+
 #[derive(MsgOnSocket, Debug)]
 pub enum VmMemoryRequest {
     /// Register shared memory represented by the given fd into guest address space. The response
@@ -259,8 +268,14 @@ pub enum VmMemoryRequest {
     /// Similiar to `VmMemoryRequest::RegisterMemory`, but doesn't allocate new address space.
     /// Useful for cases where the address space is already allocated (PCI regions).
     RegisterFdAtPciBarOffset(Alloc, MaybeOwnedFd, usize, u64),
-    /// Unregister the given memory slot that was previously registereed with `RegisterMemory`.
+    /// Similar to RegisterFdAtPciBarOffset, but is for buffers in the current
+    /// address space and requiores that NonSocketExpressibleRequests has exactly 1 pending request
+    RegisterPendingHostPointerAtPciBarOffset(Alloc, u64),
+    /// Unregister the given memory slot that was previously registered with `RegisterMemory`.
     UnregisterMemory(u32),
+    /// Unregister the NonMappedDeviceMemory that was previously registered with
+    /// `RegisterHostPointerAtPciBarOffset`.
+    UnregisterHostPointerMemory(u32),
     /// Allocate GPU buffer of a given size/format and register the memory into guest address space.
     /// The response variant is `VmResponse::AllocateAndRegisterGpuMemory`
     AllocateAndRegisterGpuMemory {
@@ -287,7 +302,12 @@ impl VmMemoryRequest {
     /// This does not return a result, instead encapsulating the success or failure in a
     /// `VmMemoryResponse` with the intended purpose of sending the response back over the socket
     /// that received this `VmMemoryResponse`.
-    pub fn execute(&self, vm: &mut Vm, sys_allocator: &mut SystemAllocator) -> VmMemoryResponse {
+    pub fn execute(
+        &self,
+        vm: &mut Vm,
+        sys_allocator: &mut SystemAllocator,
+        non_socket_expr_reqs: Arc<Mutex<NonSocketExpressibleRequests>>,
+    ) -> VmMemoryResponse {
         use self::VmMemoryRequest::*;
         match *self {
             RegisterMemory(ref fd, size) => {
@@ -302,7 +322,26 @@ impl VmMemoryRequest {
                     Err(e) => VmMemoryResponse::Err(e),
                 }
             }
+            RegisterPendingHostPointerAtPciBarOffset(alloc, offset) => {
+                let mut locked_reqs = non_socket_expr_reqs.lock();
+
+                if 1 != locked_reqs.non_mapped_memory_requests.len() {
+                    panic!("Only one NonSocketExpressibleRequest outstanding allowed!");
+                }
+
+                let mem = locked_reqs.non_mapped_memory_requests.remove(0);
+                let res = match register_memory_hva(vm, sys_allocator, mem, (alloc, offset)) {
+                    Ok((pfn, slot)) => VmMemoryResponse::RegisterMemory { pfn, slot },
+                    Err(e) => VmMemoryResponse::Err(e),
+                };
+
+                res
+            }
             UnregisterMemory(slot) => match vm.remove_mmio_memory(slot) {
+                Ok(_) => VmMemoryResponse::Ok,
+                Err(e) => VmMemoryResponse::Err(e),
+            },
+            UnregisterHostPointerMemory(slot) => match vm.remove_non_mapped_device_memory(slot) {
                 Ok(_) => VmMemoryResponse::Ok,
                 Err(e) => VmMemoryResponse::Err(e),
             },
@@ -564,6 +603,21 @@ fn register_memory(
     };
 
     let slot = vm.add_mmio_memory(GuestAddress(addr), mmap, false, false)?;
+    Ok((addr >> 12, slot))
+}
+
+fn register_memory_hva(
+    vm: &mut Vm,
+    allocator: &mut SystemAllocator,
+    mem: NonMappedDeviceMemory,
+    allocation: (Alloc, u64),
+) -> Result<(u64, u32)> {
+    let addr = allocator
+        .mmio_allocator(MmioType::High)
+        .address_from_pci_offset(allocation.0, allocation.1, mem.size() as u64)
+        .map_err(|_e| SysError::new(EINVAL))?;
+
+    let slot = vm.add_non_mapped_device_memory(GuestAddress(addr), mem, false)?;
     Ok((addr >> 12, slot))
 }
 
