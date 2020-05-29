f47c6e9c6e5c34acbcbf8e88c832d94ad3e7a72e
Chih-Hung Hsieh <chh@google.com>
2020-05-29 15:48:16 -0700

    DO NOT SUBMIT, to test protobuf-codegen for protos
    * To generate cdisk_spec.rs and plugin.rs and include them.
    * Use include!(concat!(env!("OUT_DIR"), "/cdisk_spec.rs"));
      instead of symlinks.
    * Can include ${OUT_DIR}/cdisk_spec.rs, but
      has the following errors that do not happen with symlinks:
        error: an inner attribute is not permitted in this context
        error: an inner attribute is not permitted following an outer attribute
    
    Test: make
    Change-Id: I00282a09dc8b1b9d7d4b1f729c025bd76754b353

--- protos/src/lib.rs
+++ protos/src/lib.rs
@@ -2,15 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-mod generated {
-    include!(concat!(env!("OUT_DIR"), "/generated.rs"));
-}
-
 #[cfg(feature = "plugin")]
-pub mod plugin;
-
-#[cfg(feature = "trunks")]
-pub use generated::trunks;
+include!(concat!(env!("OUT_DIR"), "/plugin.rs"));
 
 #[cfg(feature = "composite-disk")]
-pub use generated::cdisk_spec;
+include!(concat!(env!("OUT_DIR"), "/cdisk_spec.rs"));
