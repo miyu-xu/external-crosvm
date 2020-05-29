e9bb627ccc0acaf8c85dddbd10702aa5f663058b
Chih-Hung Hsieh <chh@google.com>
2020-05-29 15:48:16 -0700

    DO NOT SUBMIT, include and protobuf-codegen
    * To generate cdisk_spec.rs and plugin.rs and include them.
    * Use include!(concat!(env!("OUT_DIR"), "/cdisk_spec.rs"));
      instead of symlinks.
    * Can include ${OUT_DIR}/cdisk_spec.rs, but
      has the following errors that do not happen with symlinks:
        error: an inner attribute is not permitted in this context
        error: an inner attribute is not permitted following an outer attribute
      * The errors can be removed if those #! and //! comments
        in included files are removed, but the code might get the
        should-be-suppressed warnings.
    
    Test: make
    Change-Id: I00282a09dc8b1b9d7d4b1f729c025bd76754b353

--- protos/src/lib.rs
+++ protos/src/lib.rs
@@ -2,15 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-mod generated {
-    include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+#[cfg(feature = "plugin")]
+pub mod plugin {
+  // needs to comment out #! and //! comments in included files.
+  include!(concat!(env!("OUT_DIR"), "/plugin.rs"));
 }
 
-#[cfg(feature = "plugin")]
-pub mod plugin;
-
-#[cfg(feature = "trunks")]
-pub use generated::trunks;
-
 #[cfg(feature = "composite-disk")]
-pub use generated::cdisk_spec;
+pub mod cdisk_spec {
+  // needs to comment out #! and //! comments in included files.
+  include!(concat!(env!("OUT_DIR"), "/cdisk_spec.rs"));
+}
+
+// This does not compile: unexpected token concat
+// #[path = concat!(env!("OUT_DIR"), "/plugin.rs")]
+// pub mod cdisk_spec;
+
+
+// This works, just like symbolic link in ./cdisk_spec.rs to the generated file.
+// #[path = "../../../../out/soong/.intermediates/external/crosvm/protos/crosvm_cdisk_spec_proto/gen/cdisk_spec.rs"]
+// pub mod cdisk_spec;
