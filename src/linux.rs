cd2aa6d41f9985c0772ca105d5e9aa0410e23c87
Ram Muthiah <rammuthiah@google.com>
2020-10-28 14:18:07 -0700

    WIP: Added debug PC trace of aarch64 cpu
    Change-Id: If2a623f3277d6e2337f6b0b836e7b99b97056f4d

--- src/linux.rs
+++ src/linux.rs
@@ -1758,7 +1758,7 @@ where
 
             loop {
                 let mut interrupted_by_signal = false;
-                match vcpu.run(&vcpu_run_handle) {
+                match vcpu.run(&vcpu_run_handle, has_bios) {
                     Ok(VcpuExit::IoIn { port, mut size }) => {
                         let mut data = [0; 8];
                         if size > data.len() {
