59a33fb4007d49e2ab4f961d9d7356f02819e81c
Jason Macnak <natsu@google.com>
2021-03-04 10:16:39 -0800

    Handle rutabaga gfx update
    ... by temporarily disabling having both renderers running at the
    same time and by adding virgl feature flag to the build.
    
    Bug: b/181869105
    Test: launch_cvd --gpu_mode=gfxstream
    Test: launch_cvd --gpu_mode=virgl_renderer
    Change-Id: I2f21bfc4b7dcc205467973f32aa5a9eef89f9f56

--- rutabaga_gfx/src/rutabaga_core.rs
+++ rutabaga_gfx/src/rutabaga_core.rs
@@ -689,8 +689,9 @@ impl RutabagaBuilder {
             let rutabaga_2d = Rutabaga2D::init()?;
             rutabaga_components.insert(RutabagaComponentType::Rutabaga2D, rutabaga_2d);
         } else {
+
             #[cfg(feature = "virgl_renderer")]
-            {
+            if self.default_component == RutabagaComponentType::VirglRenderer {
                 let virglrenderer_flags = self
                     .virglrenderer_flags
                     .ok_or(RutabagaError::InvalidRutabagaBuild)?;
@@ -700,7 +701,7 @@ impl RutabagaBuilder {
             }
 
             #[cfg(feature = "gfxstream")]
-            {
+            if self.default_component == RutabagaComponentType::Gfxstream {
                 let display_width = self
                     .display_width
                     .ok_or(RutabagaError::InvalidRutabagaBuild)?;
