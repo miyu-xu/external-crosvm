202339a1a1919e2ef600b0caadba2d90feb34c26
Frederick Mayle <fmayle@google.com>
2022-06-15 18:17:10 +0000

    crosvm: Use linux/input-event-codes.h in display_wl.c
    This brings us closer to upstream.
    
    We have to use bionic's headers on host (i.e. when using glibc) which is
    not ideal, but it is done elsewhere in android (e.g. hwbinder), so it is
    probably safe.
    
    Test: TH
    Test: C2A="..." ./run_c2a.sh gpu_display
    Bug: 228881829
    Change-Id: I144a8bfd57c9300d4e79203d4896bad6508d0ba9

--- gpu_display/src/display_wl.c
+++ gpu_display/src/display_wl.c
@@ -11,11 +11,7 @@
 
 #include <errno.h>
 #include <fcntl.h>
-// Android edit:
-// #include <linux/input-event-codes.h>
-#ifndef BTN_LEFT
-#define BTN_LEFT 0x110
-#endif
+#include <linux/input-event-codes.h>
 #include <poll.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
