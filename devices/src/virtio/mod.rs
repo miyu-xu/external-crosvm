f2c06ce5525fe6b5c983da228e4e4b8a6601c449
Sreenad Menon <quic_sreemeno@quicinc.com>
2022-11-04 09:31:03 +0530

    crosvm: Add vhost scmi support
    ARM System Control and Management Interface (SCMI) protocol provides
    support to host to control/manage resources like clocks and regulators
    managed by the firmware.
    
    There is a new backend implementaion [1] of this SCMI protocol in the host
    that allows the resources (clocks/regulators) to be para-virtualized for the
    guest VMs. The guest can communicate its resource needs using SCMI protocol with
    the host over virtio transport using vhost interface between host and VMM.
    
    This patchset adds backend support of vhost-scmi to crosvm to facilitate the
    new virtio SCMI device creation and to configure the vhost interface to allow
    data transfers between guest and host over virtio.
    
    [1] - https://lore.kernel.org/linux-arm-kernel/20220609071956.5183-1-quic_neeraju@quicinc.com/
    
    Change-Id: I8fc3a520fbf5c485b60427fcec685d016db11895
    Signed-off-by: Sreenad Menon <quic_sreemeno@quicinc.com>
    Signed-off-by: Sahitya Tummala <quic_stummala@quicinc.com>

--- devices/src/virtio/mod.rs
+++ devices/src/virtio/mod.rs
@@ -133,6 +133,7 @@ pub enum DeviceType {
     Wl = virtio_ids::VIRTIO_ID_WL,
     Tpm = virtio_ids::VIRTIO_ID_TPM,
     VhostUser = virtio_ids::VIRTIO_ID_VHOST_USER,
+    Scmi = virtio_ids::VIRTIO_ID_SCMI,
 }
 
 /// Prints a string representation of the given virtio device type.
@@ -163,6 +164,7 @@ impl std::fmt::Display for DeviceType {
             DeviceType::VideoDec => write!(f, "video-decoder"),
             DeviceType::VideoEnc => write!(f, "video-encoder"),
             DeviceType::Mac80211HwSim => write!(f, "mac-80211-hw-sim"),
+            DeviceType::Scmi => write!(f, "scmi"),
         }
     }
 }
