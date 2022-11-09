bf5e6048f0aeaec74f82c0a9b1de109940e9fc7e
Frederick Mayle <fmayle@google.com>
2022-11-09 11:25:50 -0800

    ANDROID: virtio: block: use only one virtqueue in BlockAsync
    This is a temporary workaround while we debug further. Anecdotally, it
    reduces the rate of DEVICE_AVAILABLE failures in test infra.
    
    Bug: 251366833
    Change-Id: I3c7b1ecd874d846bd4e28f80c973fa77cc584b4b

--- devices/src/virtio/block/asynchronous.rs
+++ devices/src/virtio/block/asynchronous.rs
@@ -86,7 +86,7 @@ use crate::virtio::VirtioDevice;
 use crate::virtio::Writer;
 
 const DEFAULT_QUEUE_SIZE: u16 = 256;
-pub const DEFAULT_NUM_QUEUES: u16 = 16;
+pub const DEFAULT_NUM_QUEUES: u16 = 1;
 
 const SECTOR_SHIFT: u8 = 9;
 const SECTOR_SIZE: u64 = 0x01 << SECTOR_SHIFT;
