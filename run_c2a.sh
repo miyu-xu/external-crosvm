d864379589714709895a5d452baa84e54ad5f964
Frederick Mayle <fmayle@google.com>
2022-12-05 14:54:51 -0800

    ANDROID: update build scripts for new cargo2android tool
    Bug: 260604615
    Test: ran it
    Change-Id: I94fab94ec4155a2ea7b499faaee893585784b9be

--- run_c2a.sh
+++ run_c2a.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
-# Run cargo2android.py with the appropriate arguments for the crate in the
-# supplied directory.
+# Run cargo2android with the appropriate arguments.
 
 set -e -u
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 CRATE_DIRECTORY"
+if [ "$#" -ne 0 ]; then
+  echo "Usage: $0"
   exit 1
 fi
-cd $1
 
 if ! [ -x "$(command -v bpfmt)" ]; then
   echo 'Error: bpfmt not found.' >&2
   exit 1
-fi
-
-# C2A gives visibility to all APEXs by default. Restrict to "platform" (the
-# Soong default).
-C2A_ARGS="--apex-available //apex_available:platform"
-if [[ -f "cargo2android.json" ]]; then
-  # If the crate has a cargo2android config, let it handle all the flags.
-  C2A_ARGS+=" --config cargo2android.json"
-else
-  # Otherwise, set common flags.
-  C2A_ARGS+=" --run --device --tests --global_defaults=crosvm_defaults --add_workspace"
-  # If there are subdirectories with crates, then pass --no-subdir.
-  if [ -n "$(find . -mindepth 2 -name "Cargo.toml")" ]; then
-    C2A_ARGS+=" --no-subdir"
-  fi
 fi
 
 # Tell C2A to use the specific rust version that crosvm upstream expects.
@@ -40,20 +23,25 @@ fi
 RUST_TOOLCHAIN="1.62.0"
 rustup which --toolchain $RUST_TOOLCHAIN cargo || \
   rustup toolchain install $RUST_TOOLCHAIN
-C2A_ARGS+=" --cargo_bin $(dirname $(rustup which --toolchain $RUST_TOOLCHAIN cargo))"
+CARGO_BIN="$(dirname $(rustup which --toolchain $RUST_TOOLCHAIN cargo))"
 
-C2A=${C2A:-cargo2android.py}
-echo "Processing \"$1\" using $C2A $C2A_ARGS"
-$C2A $C2A_ARGS
-rm -f cargo.out
+rm -f cargo.out cargo.metadata
 rm -rf target.tmp || /bin/true
 
-if [[ -f "Android.bp" ]]; then
-  bpfmt -w Android.bp || /bin/true
-fi
+set -x
+cargo2android --cfg cargo2android.json --cargo-bin "$CARGO_BIN"
+set +x
+
+rm -f cargo.out cargo.metadata
+rm -rf target.tmp || /bin/true
+
+# Revert changes to Cargo.lock caused by cargo2android.
+#
+# Android diffs in Cargo.toml files can cause diffs in the Cargo.lock when
+# cargo2android runs. This didn't happen with cargo2android.py because it
+# ignored the lock file.
+git restore Cargo.lock
 
 # Fix workstation specific path in "metrics" crate's generated files.
 # TODO(b/232150148): Find a better solution for protobuf generated files.
-if [[ `basename $1` == "metrics" ]]; then
-  sed --in-place 's/path = ".*\/out/path = "./' out/generated.rs
-fi
+sed --in-place 's/path = ".*\/out/path = "./' metrics/out/generated.rs
