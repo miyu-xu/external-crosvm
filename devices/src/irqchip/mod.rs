092303e5eeb328f12d71b9db9be109493e8a3c0e
Jerry Wang <ze-yu.wang@mediatek.com>
2022-12-08 15:26:20 +0800

    [RFC v0.1] crosvm: hypervisor: implement GenieZone hypervisor
    To support GenieZone (gz) hypervisor as a crosvm hypervisor backend:
    - Implement GenieZone hypervisor
    - Implement GenieZone’s irqchip
    - Create config, feature for GenieZone
    - enable gz in Android.bp
    
    Now the guest VM can output booting log to uart console but not yet
    complete booting.
    
    We still have many todos:
    
    - Virtual interrupts injection
    - replace KVM’s ioctl number with GZVM’s ioctl in bindings
    - automatically probe proper hypervisor backend
    - Protected VM and pvmfw
    - Multi-core VM supports
    
    Bug:
    
    Test: ./crosvm run --gz-device='/dev/gzvm' --disable-sandbox -m 512 -s s1 \
        --rwroot alpine-rootfs.img -p 'init=/bin/sh'--display-window-keyboard \
        Image-5.15.41-nokaslr --serial type=stdout,hardware=serial,earlycon \
        --serial type=stdout,hardware=virtio-console,console,stdin
    
    Signed-off-by: Ze-yu Wang <ze-yu.wang@mediatek.com>
    Signed-off-by: Yi-de Wu <yi-de.wu@mediatek.com>
    Change-Id: I40340a341cc0997ba9b6b2280f2a1fa5e4f48f7f

--- devices/src/irqchip/mod.rs
+++ devices/src/irqchip/mod.rs
@@ -51,6 +51,13 @@ cfg_if::cfg_if! {
     }
 }
 
+#[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
+#[cfg(feature = "gz")]
+mod gz;
+#[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
+#[cfg(feature = "gz")]
+pub use self::gz::GzKernelIrqChip;
+
 pub type IrqEventIndex = usize;
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
