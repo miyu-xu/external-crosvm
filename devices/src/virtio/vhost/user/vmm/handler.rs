d37658689697b9f210eb546e638af9feaa201a16
JaeMan Park <jaeman@google.com>
2022-12-13 17:59:56 +0900

    Force calling interrupt when msix is not supported
    Bug: 261814790
    Test: N/A
    
    Change-Id: Ifca6ee1ff98fd40378c9a95df8099b28967b2924

--- devices/src/virtio/vhost/user/vmm/handler.rs
+++ devices/src/virtio/vhost/user/vmm/handler.rs
@@ -263,12 +263,18 @@ impl VhostUserHandler {
             .ok_or(Error::MsixConfigUnavailable)?;
         let msix_config = msix_config_opt.lock();
 
+        let non_msix_evt = Event::new().unwrap();
         for (queue_index, queue) in queues.iter().enumerate() {
             let queue_evt = &queue_evts[queue_index];
-            let irqfd = msix_config
-                .get_irqfd(queue.vector() as usize)
-                .unwrap_or_else(|| interrupt.get_interrupt_evt());
-            self.activate_vring(&mem, queue_index, queue, queue_evt, irqfd)?;
+            let irqfd = msix_config.get_irqfd(queue.vector() as usize);
+            match irqfd {
+                Some(irqfd_evt) => {
+                    self.activate_vring(&mem, queue_index, queue, queue_evt, irqfd_evt)?;
+                }
+                None => {
+                    self.activate_vring(&mem, queue_index, queue, queue_evt, &non_msix_evt)?;
+                }
+            }
         }
 
         drop(msix_config);
@@ -294,6 +300,7 @@ impl VhostUserHandler {
                     queues,
                     mem,
                     kill_evt,
+                    non_msix_evt,
                     backend_req_handler,
                 };
 
