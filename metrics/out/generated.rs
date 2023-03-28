5655b8e38fd8a00e399ef3256ff1ff52bc060d51
Jeff Vander Stoep <jeffv@google.com>
2023-03-28 18:43:55 +0200

    Use protobuf 2.x crate
    This keeps CrosVM on the same version as the rest of Android
    updates to 3.x.
    
    Test: TH
    Test: cargo_embargo --cfg cargo_embargo.json
    Bug: 270895633
    Change-Id: Ic013676efbd0561e565f3da715b65ea3e952c17b

--- metrics/out/generated.rs
+++ metrics/out/generated.rs
@@ -1,2 +1,2 @@
-#[path = "./event_details.rs"]
+#[path = "/usr/local/google/home/jeffv/android/aosp/external/crosvm/target.tmp/x86_64-unknown-linux-gnu/debug/build/metrics-4a07e891ea8f3d87/out/event_details.rs"]
 pub mod event_details;
