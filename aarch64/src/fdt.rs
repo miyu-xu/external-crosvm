google/crosvm

commit 653f62a4a3add83d4d4623f4c6a818b33ae341f5

aarch64: static swiotlb allocation
Adds a new hypervisor capability that causes crosvm to statically
allocate the location of the swiotlb buffer.

This will be used by the gunyah hypervisor.

BUG=b:244630071,b:232360323

Change-Id: Ia2f379bb6f2fa89167ddc73d65ec1b8c5494bdf6
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4197001
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Vikram Auradkar <auradkar@google.com>
Commit-Queue: Frederick Mayle <fmayle@google.com>

aarch64/src/fdt.rs
@@ -79,13 +79,24 @@ fn create_memory_node(fdt: &mut FdtWriter, guest_mem: &GuestMemory) -> Result<()
     Ok(())
 }
 
-fn create_resv_memory_node(fdt: &mut FdtWriter, resv_size: u64) -> Result<u32> {
+fn create_resv_memory_node(
+    fdt: &mut FdtWriter,
+    resv_addr_and_size: (Option<GuestAddress>, u64),
+) -> Result<u32> {
+    let (resv_addr, resv_size) = resv_addr_and_size;
+
     let resv_memory_node = fdt.begin_node("reserved-memory")?;
     fdt.property_u32("#address-cells", 0x2)?;
     fdt.property_u32("#size-cells", 0x2)?;
     fdt.property_null("ranges")?;
 
-    let restricted_dma_pool = fdt.begin_node("restricted_dma_reserved")?;
+    let restricted_dma_pool = if let Some(resv_addr) = resv_addr {
+        let node = fdt.begin_node(&format!("restricted_dma_reserved@{:x}", resv_addr.0))?;
+        fdt.property_array_u64("reg", &[resv_addr.0, resv_size])?;
+        node
+    } else {
+        fdt.begin_node("restricted_dma_reserved")?
+    };
     fdt.property_u32("phandle", PHANDLE_RESTRICTED_DMA_POOL)?;
     fdt.property_string("compatible", "restricted-dma-pool")?;
     fdt.property_u64("size", resv_size)?;
@@ -520,7 +531,7 @@ fn create_vmwdt_node(fdt: &mut FdtWriter, vmwdt_cfg: VmWdtConfig) -> Result<()> 
 /// * `android_fstab` - An optional file holding Android fstab entries
 /// * `is_gicv3` - True if gicv3, false if v2
 /// * `psci_version` - the current PSCI version
-/// * `swiotlb` - Reserve a memory pool for DMA
+/// * `swiotlb` - Reserve a memory pool for DMA. Tuple of base address and size.
 /// * `bat_mmio_base_and_irq` - The battery base address and irq number
 /// * `vmwdt_cfg` - The virtual watchdog configuration
 /// * `dump_device_tree_blob` - Option path to write DTB to
@@ -541,7 +552,7 @@ pub fn create_fdt(
     is_gicv3: bool,
     use_pmu: bool,
     psci_version: PsciVersion,
-    swiotlb: Option<u64>,
+    swiotlb: Option<(Option<GuestAddress>, u64)>,
     bat_mmio_base_and_irq: Option<(u64, u32)>,
     vmwdt_cfg: VmWdtConfig,
     dump_device_tree_blob: Option<PathBuf>,

aarch64/src/lib.rs
@@ -151,6 +151,19 @@ fn get_bios_addr() -> GuestAddress {
     GuestAddress(AARCH64_PHYS_MEM_START + AARCH64_BIOS_OFFSET)
 }
 
+// When static swiotlb allocation is required, returns the address it should be allocated at.
+// Otherwise, returns None.
+fn get_swiotlb_addr(
+    memory_size: u64,
+    hypervisor: &(impl Hypervisor + ?Sized),
+) -> Option<GuestAddress> {
+    if hypervisor.check_capability(HypervisorCap::StaticSwiotlbAllocationRequired) {
+        Some(GuestAddress(AARCH64_PHYS_MEM_START + memory_size))
+    } else {
+        None
+    }
+}
+
 // Serial device requires 8 bytes of registers;
 const AARCH64_SERIAL_SIZE: u64 = 0x8;
 // This was the speed kvmtool used, not sure if it matters.
@@ -312,7 +325,7 @@ impl arch::LinuxArch for AArch64 {
     /// These should be used to configure the GuestMemory structure for the platform.
     fn guest_memory_layout(
         components: &VmComponents,
-        _hypervisor: &impl Hypervisor,
+        hypervisor: &impl Hypervisor,
     ) -> std::result::Result<Vec<(GuestAddress, u64)>, Self::Error> {
         let mut memory_regions =
             vec![(GuestAddress(AARCH64_PHYS_MEM_START), components.memory_size)];
@@ -323,6 +336,12 @@ impl arch::LinuxArch for AArch64 {
                 GuestAddress(AARCH64_PROTECTED_VM_FW_START),
                 AARCH64_PROTECTED_VM_FW_MAX_SIZE,
             ));
+        }
+
+        if let Some(size) = components.swiotlb {
+            if let Some(addr) = get_swiotlb_addr(components.memory_size, hypervisor) {
+                memory_regions.push((addr, size));
+            }
         }
 
         Ok(memory_regions)
@@ -643,7 +662,12 @@ impl arch::LinuxArch for AArch64 {
             irq_chip.get_vgic_version() == DeviceKind::ArmVgicV3,
             use_pmu,
             psci_version,
-            components.swiotlb,
+            components.swiotlb.map(|size| {
+                (
+                    get_swiotlb_addr(components.memory_size, vm.get_hypervisor()),
+                    size,
+                )
+            }),
             bat_mmio_base_and_irq,
             vmwdt_cfg,
             dump_device_tree_blob,

arch/src/lib.rs
@@ -399,7 +399,7 @@ pub trait LinuxArch {
     /// * `components` - Parts used to determine the memory layout.
     fn guest_memory_layout(
         components: &VmComponents,
-        _hypervisor: &impl hypervisor::Hypervisor,
+        hypervisor: &impl hypervisor::Hypervisor,
     ) -> std::result::Result<Vec<(GuestAddress, u64)>, Self::Error>;
 
     /// Gets the configuration for a new `SystemAllocator` that fits the given `Vm`'s memory layout.

hypervisor/src/caps.rs
@@ -24,6 +24,12 @@ pub enum HypervisorCap {
     /// capability, which causes crosvm to substitute a calibrated value in leaf
     /// 0x15 that will be accurate enough for use in a clocksource.
     CalibratedTscLeafRequired,
+    // By default, when swiotlb is enabled, crosvm will only specify its size in the device tree
+    // and allow the guest to decide where to allocate the buffer in guest phsyical memory.
+    //
+    // If this capability is declared, then instead crosvm will allocate space for the swiotlb
+    // outside of guest physical memory and specify both the address and size in the device tree.
+    StaticSwiotlbAllocationRequired,
 }
 
 /// A capability the `Vm` can possibly expose.

hypervisor/src/kvm/mod.rs
@@ -1222,6 +1222,7 @@ impl TryFrom<HypervisorCap> for KvmCap {
             HypervisorCap::Xcrs => Ok(KvmCap::Xcrs),
             #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
             HypervisorCap::CalibratedTscLeafRequired => Err(Error::new(libc::EINVAL)),
+            HypervisorCap::StaticSwiotlbAllocationRequired => Err(Error::new(libc::EINVAL)),
         }
     }
 }

miyu-xu/external-crosvm

commit b33d505dda0a754cb2fe4270f423aef80c289ba8

aarch64: Fix size prop in swiotlb
Devicetree bindings for restricted-dma-pool say that *either* "size" or
"reg" property should be used (but do not allow for both).

  Each child node may either use a 'reg' property ... or a 'size'
  property ...

Presently, "size" property is added even when fixed swiotlb region is
used. Apply the "size" property only when using dynamically allocated
swiotlb.

BUG=b:244630071

Fixes: 653f62a4a3ad ("aarch64: static swiotlb allocation")
Change-Id: I1c88c9be043133e698e1da9072e5353ad7419e27
Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

aarch64/src/fdt.rs
@@ -97,10 +97,10 @@ fn create_resv_memory_node(
         node
     } else {
         fdt.begin_node("restricted_dma_reserved")?
+        fdt.property_u64("size", resv_size)?;
     };
     fdt.property_u32("phandle", PHANDLE_RESTRICTED_DMA_POOL)?;
     fdt.property_string("compatible", "restricted-dma-pool")?;
-    fdt.property_u64("size", resv_size)?;
     fdt.property_u64("alignment", base::pagesize() as u64)?;
     fdt.end_node(restricted_dma_pool)?;
 