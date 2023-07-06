e68ea21c81432de4ecd4aef2f4df2decee8058bb
Frederick Mayle <fmayle@google.com>
2023-07-05 17:45:28 -0700

    DO NOT SUBMIT: snapshot demo hacks
    Bug: n/a
    Test: n/a
    Change-Id: I2a55b7e5c6f7212de068845e32b515695678dbfc

--- src/crosvm/sys/unix.rs
+++ src/crosvm/sys/unix.rs
@@ -3637,7 +3637,8 @@ fn irq_handler_thread(
                         }
                         Err(e) => {
                             if let TubeError::Disconnected = e {
-                                panic!("irq handler control tube disconnected.");
+                                error!("irq handler control tube disconnected.");
+                                return Ok(());
                             } else {
                                 error!("failed to recv IrqHandlerRequest: {}", e);
                             }
@@ -3827,7 +3828,8 @@ fn vm_memory_handler_thread(
                     },
                     Err(e) => {
                         if let TubeError::Disconnected = e {
-                            panic!("vm memory control tube disconnected.");
+                            error!("vm memory control tube disconnected.");
+                            return Ok(());
                         } else {
                             error!("failed to recv VmMemoryHandlerRequest: {}", e);
                         }
