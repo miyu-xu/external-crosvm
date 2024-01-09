ce29eb8152b1145e3900f6aef40c833fa86592c8
Elie Kheirallah <khei@google.com>
2024-01-09 22:38:09 +0000

    DO NOT SUBMIT: restore MSRs 1 at a time without failing
    Change-Id: I34490a4ff274bbeb8cd323b3887c4ac85d285978

--- hypervisor/src/x86_64.rs
+++ hypervisor/src/x86_64.rs
@@ -11,6 +11,7 @@ use std::arch::x86_64::_rdtsc;
 use base::custom_serde::deserialize_seq_to_arr;
 use base::custom_serde::serialize_arr;
 use base::error;
+use base::info;
 use base::Result;
 use bit_field::*;
 use downcast_rs::impl_downcast;
@@ -178,8 +179,11 @@ pub trait VcpuX86_64: Vcpu {
         self.set_debugregs(&snapshot.debug_regs)?;
         self.set_xcrs(&snapshot.xcrs)?;
         self.set_msrs(&snapshot.msrs)?;
+        info!("SET MSRS successfully");
         self.set_xsave(&snapshot.xsave)?;
+        info!("SET Xsave successfully");
         self.set_interrupt_state(snapshot.hypervisor_data.clone())?;
+        info!("SET interrupts successfully");
         self.restore_timekeeping(host_tsc_reference_moment, snapshot.tsc_offset)?;
         Ok(())
     }
