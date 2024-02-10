de133f92b86ceef93b0e2aa00966459b88cf5e8e
David Dai <davidai@google.com>
2024-02-09 17:48:12 -0800

    devices: virtcpufreq: Finish proxy syscall support through UDS
    Add the remaining implementation to communicate via sockets to make
    syscalls using privileged proxy services for virtcpufreq. This acts a
    fallback mechanism for systems that do not grant CAP_SYS_NICE for
    crosvm.
    
    Bug: 284367589
    Test: Booted up and launched microdroid
    Change-Id: I3b29565f533345ae123f0bb6e963487e0160e3f2
    Signed-off-by: David Dai <davidai@google.com>

--- devices/src/virtcpufreq.rs
+++ devices/src/virtcpufreq.rs
@@ -3,13 +3,15 @@
 // found in the LICENSE file.
 
 use base::sched_attr;
-use base::sched_setattr;
+use base::sys::platform::sched_attr;
 use base::warn;
 use base::Error;
+use std::io::Write;
 use std::os::unix::net::UnixStream;
 use std::sync::Arc;
 
 use sync::Mutex;
+use zerocopy::AsBytes;
 
 use crate::pci::CrosvmDeviceId;
 use crate::BusAccessInfo;
@@ -28,6 +30,7 @@ pub struct VirtCpufreq {
     cpu_fmax: u32,
     cpu_capacity: u32,
     pcpu: u32,
+    socket: Option<Arc<Mutex<UnixStream>>>,
 }
 
 fn get_cpu_info(cpu_id: u32, property: &str) -> Result<u32, Error> {
@@ -51,7 +54,7 @@ fn get_cpu_curfreq_khz(cpu_id: u32) -> Result<u32, Error> {
 }
 
 impl VirtCpufreq {
-    pub fn new(pcpu: u32, _socket: Option<Arc<Mutex<UnixStream>>>) -> Self {
+    pub fn new(pcpu: u32, socket: Option<Arc<Mutex<UnixStream>>>) -> Self {
         let cpu_capacity = get_cpu_capacity(pcpu).expect("Error reading capacity");
         let cpu_fmax = get_cpu_maxfreq_khz(pcpu).expect("Error reading max freq");
 
@@ -59,6 +62,7 @@ impl VirtCpufreq {
             cpu_fmax,
             cpu_capacity,
             pcpu,
+            socket,
         }
     }
 }
@@ -106,15 +110,26 @@ impl BusDevice for VirtCpufreq {
 
         // Undo 25% margin applied by schedutil governor to cpufreq.
         let cpu_cap_scaled = self.cpu_capacity * 80 / 100;
-        let util = cpu_cap_scaled * freq / self.cpu_fmax;
+        let util = self.cpu_capacity * freq / self.cpu_fmax;
 
-        let mut sched_attr = sched_attr::default();
-        sched_attr.sched_flags =
-            SCHED_FLAG_KEEP_ALL | SCHED_FLAG_UTIL_CLAMP_MIN | SCHED_FLAG_RESET_ON_FORK;
-        sched_attr.sched_util_min = util;
+        if let Some(socket) = &self.socket {
+            let tid = base::linux::gettid();
+            // util <= 1024, see include/linux/sched.h in Linux
+            // tid <= 65536, see bionic/README.md in Android
+            let msg = (tid as u32 & 0xFFFF) | (util & 0xFFFF) << 16;
+            socket
+                .lock()
+                .write_all(msg.as_bytes())
+                .expect("failed to write to socket");
+        } else {
+            let mut sched_attr = sched_attr::default();
+            sched_attr.sched_flags =
+                SCHED_FLAG_KEEP_ALL | SCHED_FLAG_UTIL_CLAMP_MIN | SCHED_FLAG_RESET_ON_FORK;
+            sched_attr.sched_util_min = util;
 
-        if let Err(e) = sched_setattr(0, &mut sched_attr, 0) {
-            panic!("{}: Error setting util value: {}", self.debug_label(), e);
+            if let Err(e) = sched_setattr(0, &mut sched_attr, 0) {
+                panic!("{}: Error setting util value: {}", self.debug_label(), e);
+            }
         }
     }
 }
