0de8568a18254896f598133d36967bc18f3ee954
Mu-Le Lee <mulelee@google.com>
2024-04-22 17:03:58 +0800

    Implement virtio-snd aaudio stream to support audio in android
    note: this CL is uploaded to aosp for reviewer. planned to upstream to
    crosvm.
    This change introduces feature audio_aaudio on top of the support of
    virtio-snd-multiple-backend.
    
    Test: Play Youtube video on panther
    Test: cargo build -F aaudio_backend
    Test: ./tools/run_tests
    Test: ./tools/run_tests -F aaudio_backend
    Test: ./tools/presubmit
    Bug: 325930215
    Change-Id: I7c275433bf94d4a347720ce0ffa9127ffcb66a1e

---
+++ aaudio_backend/src/aaudio_backend.rs
@@ -0,0 +1,15 @@
+// Copyright 2024 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+pub fn init(_num_channels: usize, _framte_rate: u32) {
+    unimplemented!();
+}
+
+pub fn playback(_buffer: &mut [u8], _num_frame: usize) {
+    unimplemented!();
+}
+
+pub fn release() {
+    unimplemented!();
+}
