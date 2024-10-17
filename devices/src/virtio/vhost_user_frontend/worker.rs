google/crosvm

commit 8de3640c01bfb09ea0069b1dedd9118781c819e6

devices: vhost-user frontend: rewrite worker as non-async
Also get rid of the platform-specific worker code in sys, since we can
use ReadNotifier and CloseNotifier to implement the worker in a way that
works on Linux and Windows.

BUG=b:370853464

Change-Id: I14e7f33a584728f9cf54ab807a32b2655b119749
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5901870
Reviewed-by: Noah Gold <nkgold@google.com>
Reviewed-by: Frederick Mayle <fmayle@google.com>
Commit-Queue: Yuan Yao <yuanyaogoog@chromium.org>

devices/src/virtio/vhost_user_frontend/worker.rs
@@ -2,19 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use std::pin::pin;
-
+use anyhow::bail;
 use anyhow::Context;
+use base::info;
+#[cfg(windows)]
+use base::CloseNotifier;
 use base::Event;
-use cros_async::EventAsync;
-use cros_async::Executor;
-use futures::channel::oneshot;
-use futures::select_biased;
-use futures::FutureExt;
+use base::EventToken;
+use base::EventType;
+use base::ReadNotifier;
+use base::WaitContext;
+use vmm_vhost::Error as VhostError;
 
-use crate::virtio::async_utils;
 use crate::virtio::vhost_user_frontend::handler::BackendReqHandler;
-use crate::virtio::vhost_user_frontend::sys::run_backend_request_handler;
 use crate::virtio::Interrupt;
 use crate::virtio::VIRTIO_MSI_NO_VECTOR;
 
@@ -25,61 +25,110 @@ pub struct Worker {
 }
 
 impl Worker {
-    // Runs asynchronous tasks.
-    pub async fn run(&mut self, ex: &Executor, interrupt: Interrupt) -> anyhow::Result<()> {
-        let non_msix_evt = self
-            .non_msix_evt
-            .try_clone()
-            .expect("failed to clone non_msix_evt");
-        let mut handle_non_msix_evt =
-            pin!(handle_non_msix_evt(ex, non_msix_evt, interrupt.clone()).fuse());
-
-        let mut resample = pin!(async_utils::handle_irq_resample(ex, interrupt).fuse());
-
-        let kill_evt = self.kill_evt.try_clone().expect("failed to clone kill_evt");
-        let mut kill = pin!(async_utils::await_and_exit(ex, kill_evt).fuse());
-
-        let (stop_tx, stop_rx) = oneshot::channel();
-        let mut req_handler = pin!(if let Some(backend_req_handler) =
-            self.backend_req_handler.as_mut()
-        {
-            run_backend_request_handler(ex, backend_req_handler, stop_rx)
-                .fuse()
-                .left_future()
-        } else {
-            stop_rx.map(|_| Ok(())).right_future()
+    pub fn run(&mut self, interrupt: Interrupt) -> anyhow::Result<()> {
+        #[derive(EventToken)]
+        enum Token {
+            Kill,
+            NonMsixEvt,
+            Resample,
+            ReqHandlerRead,
+            ReqHandlerClose,
         }
-        .fuse());
 
-        select_biased! {
-            r = kill => {
-                r.context("failed to wait on the kill event")?;
-                // Stop req_handler cooperatively.
-                let _ = stop_tx.send(());
-                req_handler.await.context("backend request failure on stop")?;
+        let wait_ctx = WaitContext::build_with(&[
+            (&self.non_msix_evt, Token::NonMsixEvt),
+            (&self.kill_evt, Token::Kill),
+        ])
+        .context("failed to build WaitContext")?;
+
+        if let Some(resample_evt) = interrupt.get_resample_evt() {
+            wait_ctx
+                .add(resample_evt, Token::Resample)
+                .context("failed to add resample event to WaitContext")?;
+        }
+
+        if let Some(backend_req_handler) = self.backend_req_handler.as_mut() {
+            wait_ctx
+                .add(
+                    backend_req_handler.get_read_notifier(),
+                    Token::ReqHandlerRead,
+                )
+                .context("failed to add backend req handler to WaitContext")?;
+
+            #[cfg(any(target_os = "android", target_os = "linux"))]
+            wait_ctx
+                .add_for_event(
+                    backend_req_handler.get_read_notifier(),
+                    EventType::None, // only get hangup events from the close notifier
+                    Token::ReqHandlerClose,
+                )
+                .context("failed to add backend req handler close notifier to WaitContext")?;
+            #[cfg(target_os = "windows")]
+            wait_ctx
+                .add(
+                    backend_req_handler.get_close_notifier(),
+                    Token::ReqHandlerClose,
+                )
+                .context("failed to add backend req handler close notifier to WaitContext")?;
+        }
+
+        'wait: loop {
+            let events = wait_ctx.wait().context("WaitContext::wait() failed")?;
+            for event in events {
+                match event.token {
+                    Token::Kill => {
+                        break 'wait;
+                    }
+                    Token::NonMsixEvt => {
+                        // The vhost-user protocol allows the backend to signal events, but for
+                        // non-MSI-X devices, a device must also update the interrupt status mask.
+                        // `non_msix_evt` proxies events from the vhost-user backend to update the
+                        // status mask.
+                        let _ = self.non_msix_evt.wait();
+
+                        // The parameter vector of signal_used_queue is used only when msix is
+                        // enabled.
+                        interrupt.signal_used_queue(VIRTIO_MSI_NO_VECTOR);
+                    }
+                    Token::Resample => {
+                        interrupt.interrupt_resample();
+                    }
+                    Token::ReqHandlerRead => {
+                        let Some(backend_req_handler) = self.backend_req_handler.as_mut() else {
+                            continue;
+                        };
+
+                        match backend_req_handler.handle_request() {
+                            Ok(_) => (),
+                            Err(VhostError::ClientExit) | Err(VhostError::Disconnect) => {
+                                info!("backend req handler connection closed");
+                                // Stop monitoring `backend_req_handler` as the client closed
+                                // the connection.
+                                let _ = wait_ctx.delete(backend_req_handler.get_read_notifier());
+                                #[cfg(target_os = "windows")]
+                                let _ = wait_ctx.delete(backend_req_handler.get_close_notifier());
+                                self.backend_req_handler = None;
+                            }
+                            Err(e) => {
+                                bail!("failed to handle a vhost-user request: {}", e);
+                            }
+                        }
+                    }
+                    Token::ReqHandlerClose => {
+                        let Some(backend_req_handler) = self.backend_req_handler.as_mut() else {
+                            continue;
+                        };
+
+                        info!("backend req handler connection closed");
+                        let _ = wait_ctx.delete(backend_req_handler.get_read_notifier());
+                        #[cfg(target_os = "windows")]
+                        let _ = wait_ctx.delete(backend_req_handler.get_close_notifier());
+                        self.backend_req_handler = None;
+                    }
+                }
             }
-            r = handle_non_msix_evt => r.context("non msix event failure")?,
-            r = resample => r.context("failed to resample a irq value")?,
-            r = req_handler => r.context("backend request failure")?,
         }
 
         Ok(())
-    }
-}
-
-// The vhost-user protocol allows the backend to signal events, but for non-MSI-X devices,
-// a device must also update the interrupt status mask. `handle_non_msix_evt` proxies events
-// from the vhost-user backend to update the status mask.
-async fn handle_non_msix_evt(
-    ex: &Executor,
-    non_msix_evt: Event,
-    interrupt: Interrupt,
-) -> anyhow::Result<()> {
-    let event_async =
-        EventAsync::new(non_msix_evt, ex).expect("failed to create async non_msix_evt");
-    loop {
-        let _ = event_async.next_val().await;
-        // The parameter vector of signal_used_queue is used only when msix is enabled.
-        interrupt.signal_used_queue(VIRTIO_MSI_NO_VECTOR);
     }
 }

devices/src/virtio/vhost_user_frontend/mod.rs
@@ -358,20 +358,15 @@ impl VhostUserFrontend {
         }
 
         self.worker_thread = Some(WorkerThread::start(label.clone(), move |kill_evt| {
-            let ex = cros_async::Executor::new().expect("failed to create an executor");
-            let ex2 = ex.clone();
-            ex.run_until(async {
-                let mut worker = Worker {
-                    kill_evt,
-                    non_msix_evt,
-                    backend_req_handler,
-                };
-                if let Err(e) = worker.run(&ex2, interrupt).await {
-                    error!("failed to run {} worker: {:#}", label, e);
-                }
-                worker.backend_req_handler
-            })
-            .expect("run_until failed")
+            let mut worker = Worker {
+                kill_evt,
+                non_msix_evt,
+                backend_req_handler,
+            };
+            if let Err(e) = worker.run(interrupt) {
+                error!("failed to run {} worker: {:#}", label, e);
+            }
+            worker.backend_req_handler
         }));
     }
 }

devices/src/virtio/vhost_user_frontend/sys.rs
@@ -13,4 +13,3 @@ cfg_if::cfg_if! {
 }
 
 pub(in super::super) use platform::create_backend_req_handler;
-pub(super) use platform::run_backend_request_handler;

devices/src/virtio/vhost_user_frontend/sys/unix.rs
@@ -2,19 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use std::pin::pin;
-
-use anyhow::bail;
-use anyhow::Context;
-use anyhow::Result;
-use base::info;
-use base::AsRawDescriptor;
 use base::SafeDescriptor;
-use cros_async::AsyncWrapper;
-use cros_async::Executor;
-use futures::channel::oneshot;
-use futures::future::FutureExt;
-use vmm_vhost::Error as VhostError;
 use vmm_vhost::FrontendServer;
 
 use crate::virtio::vhost_user_frontend::handler::BackendReqHandler;
@@ -26,44 +14,4 @@ pub fn create_backend_req_handler(
     h: BackendReqHandlerImpl,
 ) -> VhostResult<(BackendReqHandler, SafeDescriptor)> {
     FrontendServer::with_stream(h).map_err(Error::CreateBackendReqHandler)
-}
-
-/// Process requests from the backend.
-///
-/// If `stop_rx` is sent a value, the function will exit at a well defined point so that
-/// `run_backend_request_handler` can be re-invoked to resume processing the connection.
-pub async fn run_backend_request_handler(
-    ex: &Executor,
-    handler: &mut BackendReqHandler,
-    mut stop_rx: oneshot::Receiver<()>,
-) -> Result<()> {
-    let h = SafeDescriptor::try_from(handler as &dyn AsRawDescriptor)
-        .map(AsyncWrapper::new)
-        .context("failed to get safe descriptor for handler")?;
-    let handler_source = ex
-        .async_from(h)
-        .context("failed to create an async source")?;
-
-    let mut wait_readable_future = pin!(handler_source.wait_readable().fuse());
-
-    loop {
-        futures::select_biased! {
-            _ = stop_rx => return Ok(()),
-            r = wait_readable_future => {
-                r.context("failed to wait for the handler to become readable")?;
-                match handler.handle_request() {
-                    Ok(_) => (),
-                    Err(VhostError::ClientExit) => {
-                        info!("vhost-user connection closed");
-                        // Exit as the client closed the connection.
-                        return Ok(());
-                    }
-                    Err(e) => {
-                        bail!("failed to handle a vhost-user request: {}", e);
-                    }
-                };
-                wait_readable_future.set(handler_source.wait_readable().fuse());
-            }
-        };
-    }
 }

devices/src/virtio/vhost_user_frontend/sys/windows.rs
@@ -2,20 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::Context;
-use anyhow::Result;
-use base::info;
-use base::CloseNotifier;
-use base::ReadNotifier;
 use base::SafeDescriptor;
 use base::Tube;
-use cros_async::EventAsync;
-use cros_async::Executor;
-use futures::channel::oneshot;
-use futures::pin_mut;
-use futures::select_biased;
-use futures::FutureExt;
-use vmm_vhost::message::VhostUserProtocolFeatures;
 
 use crate::virtio::vhost_user_frontend::handler::BackendReqHandler;
 use crate::virtio::vhost_user_frontend::handler::BackendReqHandlerImpl;
@@ -28,44 +16,4 @@ pub fn create_backend_req_handler(
 ) -> VhostResult<(BackendReqHandler, SafeDescriptor)> {
     let backend_pid = backend_pid.expect("tube needs target pid for backend requests");
     vmm_vhost::FrontendServer::with_tube(h, backend_pid).map_err(Error::CreateBackendReqHandler)
-}
-
-/// Process requests from the backend.
-///
-/// If `stop_rx` is sent a value, the function will exit at a well defined point so that
-/// `run_backend_request_handler` can be re-invoked to resume processing the connection.
-pub async fn run_backend_request_handler(
-    ex: &Executor,
-    handler: &mut BackendReqHandler,
-    mut stop_rx: oneshot::Receiver<()>,
-) -> Result<()> {
-    let read_notifier = handler.get_read_notifier();
-    let close_notifier = handler.get_close_notifier();
-
-    let read_event = EventAsync::clone_raw_without_reset(read_notifier, ex)
-        .context("failed to create an async event")?;
-    let close_event = EventAsync::clone_raw_without_reset(close_notifier, ex)
-        .context("failed to create an async event")?;
-
-    let read_event_fut = read_event.next_val().fuse();
-    let close_event_fut = close_event.next_val().fuse();
-    pin_mut!(read_event_fut);
-    pin_mut!(close_event_fut);
-
-    loop {
-        select_biased! {
-            _ = stop_rx => return Ok(()),
-            _read_res = read_event_fut => {
-                handler
-                    .handle_request()
-                    .context("failed to handle a vhost-user request")?;
-                read_event_fut.set(read_event.next_val().fuse());
-            }
-            // Tube closed event.
-            _close_res = close_event_fut => {
-                info!("exit run loop: got close event");
-                return Ok(())
-            }
-        }
-    }
 }

third_party/vmm_vhost/src/sys/unix.rs
@@ -16,6 +16,7 @@ use std::path::PathBuf;
 
 use base::AsRawDescriptor;
 use base::RawDescriptor;
+use base::ReadNotifier;
 use base::SafeDescriptor;
 use base::ScmSocket;
 
@@ -243,6 +244,12 @@ impl AsRawDescriptor for SocketPlatformConnection {
     }
 }
 
+impl ReadNotifier for SocketPlatformConnection {
+    fn get_read_notifier(&self) -> &dyn AsRawDescriptor {
+        &self.sock
+    }
+}
+
 impl<R: Req> TryFrom<SafeDescriptor> for Connection<R> {
     type Error = Error;
 
@@ -276,9 +283,14 @@ impl<R: Req> Connection<R> {
 }
 
 impl<S: Frontend> AsRawDescriptor for FrontendServer<S> {
-    /// Used for polling.
     fn as_raw_descriptor(&self) -> RawDescriptor {
         self.sub_sock.as_raw_descriptor()
+    }
+}
+
+impl<S: Frontend> ReadNotifier for FrontendServer<S> {
+    fn get_read_notifier(&self) -> &dyn AsRawDescriptor {
+        self.sub_sock.0.get_read_notifier()
     }
 }
 

miyu-xu/external-crosvm

commit 9907af3055b3d68b86af3c6b0667d830bec9bf4c

devices: vhost-user frontend: fix backend_req on Linux
On Linux, we don't need to monitor anything special to get close
notifications; the Token::ReqHandlerRead event will be generated when
the fd closes (with is_hungup = true) and the closure will be handled
inline.

Adding the same backend_req_handler.get_read_notifier() to the wait_ctx
again causes an error, since that fd was already added in a previous
call.

BUG=None
TEST=`crosvm device block` + `crosvm run`

Fixes: 8de3640c01bf ("devices: vhost-user frontend: rewrite worker as non-async")
Change-Id: I1318c5395670476bdc333a2bae1e8c8aba2c658e

devices/src/virtio/vhost_user_frontend/worker.rs
@@ -38,6 +38,7 @@ impl Worker {
             NonMsixEvt,
             Resample,
             ReqHandlerRead,
+            #[cfg(target_os = "windows")]
             ReqHandlerClose,
             // monitor whether backend_client_fd is broken
             BackendCloseNotify,
@@ -62,14 +63,6 @@ impl Worker {
                 )
                 .context("failed to add backend req handler to WaitContext")?;
 
-            #[cfg(any(target_os = "android", target_os = "linux"))]
-            wait_ctx
-                .add_for_event(
-                    backend_req_handler.get_read_notifier(),
-                    EventType::None, // only get hangup events from the close notifier
-                    Token::ReqHandlerClose,
-                )
-                .context("failed to add backend req handler close notifier to WaitContext")?;
             #[cfg(target_os = "windows")]
             wait_ctx
                 .add(
@@ -137,6 +130,7 @@ impl Worker {
                             }
                         }
                     }
+                    #[cfg(target_os = "windows")]
                     Token::ReqHandlerClose => {
                         let Some(backend_req_handler) = self.backend_req_handler.as_mut() else {
                             continue;
@@ -144,7 +138,6 @@ impl Worker {
 
                         info!("backend req handler connection closed");
                         let _ = wait_ctx.delete(backend_req_handler.get_read_notifier());
-                        #[cfg(target_os = "windows")]
                         let _ = wait_ctx.delete(backend_req_handler.get_close_notifier());
                         self.backend_req_handler = None;
                     }