d8d1e2119afc97cdff58fd7ab68381db03cab646
Jason Macnak <natsu@google.com>
2024-10-23 13:14:18 -0700

    rutabaga_gfx: Add context snapshot and restore
    Bug: b/369615058
    Test: cvd start \
          --gpu_mode=gfxstream_guest_angle_host_swiftshader \
          --enable_virtiofs=false
    Test: cvd snapshot_take \
          --force \
          --auto_suspend \
          --snapshot_path=/tmp/snapshot1
    Test: cvd create --snapshot_path=/tmp/snapshot1
    Change-Id: Ie2f178e1d550dc6d074f0989bd4ecbbf824f8e73

--- rutabaga_gfx/src/gfxstream.rs
+++ rutabaga_gfx/src/gfxstream.rs
@@ -11,6 +11,8 @@
 use std::convert::TryInto;
 use std::ffi::CString;
 use std::io::IoSliceMut;
+use std::io::Read;
+use std::io::Write;
 use std::mem::size_of;
 use std::os::raw::c_char;
 use std::os::raw::c_int;
@@ -34,6 +36,9 @@ use crate::rutabaga_os::IntoRawDescriptor;
 use crate::rutabaga_os::OwnedDescriptor;
 use crate::rutabaga_os::RawDescriptor;
 use crate::rutabaga_utils::*;
+
+use serde::Deserialize;
+use serde::Serialize;
 
 // See `virtgpu-gfxstream-renderer.h` for definitions
 const STREAM_RENDERER_PARAM_USER_DATA: u64 = 1;
@@ -237,6 +242,11 @@ pub struct Gfxstream {
     _cookie: Box<RutabagaCookie>,
 }
 
+#[derive(Deserialize, Serialize)]
+struct GfxstreamContextSnapshot {
+    ctx_id: u32,
+}
+
 struct GfxstreamContext {
     ctx_id: u32,
     fence_handler: RutabagaFenceHandler,
@@ -338,6 +348,16 @@ impl RutabagaContext for GfxstreamContext {
         }
 
         Ok(hnd)
+    }
+
+    fn snapshot(&self) -> RutabagaResult<Vec<u8>> {
+        let snapshot = GfxstreamContextSnapshot {
+            ctx_id: self.ctx_id,
+        };
+
+        let mut buffer = std::io::Cursor::new(Vec::new());
+        serde_json::to_writer(&mut buffer, &snapshot).map_err(|e| RutabagaError::IoError(e.into()))?;
+        Ok(buffer.into_inner())
     }
 }
 
@@ -913,6 +933,21 @@ impl RutabagaComponent for Gfxstream {
         let ret = unsafe { stream_renderer_restore(cstring.as_ptr() as *const c_char) };
         ret_to_res(ret)?;
         Ok(())
+    }
+
+    #[cfg(gfxstream_unstable)]
+    fn restore_context(
+        &self,
+        snapshot: Vec<u8>,
+        fence_handler: RutabagaFenceHandler,
+    ) -> RutabagaResult<Box<dyn RutabagaContext>> {
+        let context_snapshot: GfxstreamContextSnapshot =
+                serde_json::from_reader(&snapshot[..]).map_err(|e| RutabagaError::IoError(e.into()))?;
+
+        Ok(Box::new(GfxstreamContext {
+            ctx_id: context_snapshot.ctx_id,
+            fence_handler,
+        }))
     }
 
     #[cfg(gfxstream_unstable)]
