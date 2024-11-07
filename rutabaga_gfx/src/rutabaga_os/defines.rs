90d29075fbba24bbce24c60f52fe398db46b9fa4
Gurchetan Singh <gurchetansingh@google.com>
2024-11-07 10:00:09 -0800

    rutabaga_gfx: introduce WaitTimeout abstraction
    It's a marginally better API for the WaitContext.
    
    BUG=b:378101009
    TEST=gfxstream end2end tests
    
    Change-Id: Ideaa89710568af25096409767213cbf721c9210d

--- rutabaga_gfx/src/rutabaga_os/defines.rs
+++ rutabaga_gfx/src/rutabaga_os/defines.rs
@@ -2,11 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use std::time::Duration;
+
 use crate::rutabaga_utils::RutabagaMapping;
 
 pub enum TubeType {
     Stream,
     Packet,
+}
+
+pub enum WaitTimeout {
+    ZERO,
+    FINITE(Duration),
+    NONE,
 }
 
 pub struct WaitEvent {
