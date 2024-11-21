d585309548fd688861004281f6b45420040847ff
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-11-21 05:11:55 +0000

    Revert "UPSTREAM: virtcpufreq_v2: Place vcpu worker threads dire..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/746b4dfd-3ae6-4f88-b4e7-3265fda544fa). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I803b85ecddc440232a6ba0c8fa21e57e2f8a59ae
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- aarch64/src/lib.rs
+++ aarch64/src/lib.rs
@@ -730,30 +730,28 @@ impl arch::LinuxArch for AArch64 {
         #[cfg(any(target_os = "android", target_os = "linux"))]
         if !components.cpu_frequencies.is_empty() {
             let mut freq_domain_vcpus: BTreeMap<u32, Vec<usize>> = BTreeMap::new();
-            let mut vcpu_affinities: Vec<u32> = Vec::new();
             for vcpu in 0..vcpu_count {
                 let freq_domain = *components.vcpu_domains.get(&vcpu).unwrap_or(&(vcpu as u32));
                 freq_domain_vcpus.entry(freq_domain).or_default().push(vcpu);
+            }
+            for vcpu in 0..vcpu_count {
                 let vcpu_affinity = match components.vcpu_affinity.clone() {
                     Some(VcpuAffinity::Global(v)) => v,
                     Some(VcpuAffinity::PerVcpu(mut m)) => m.remove(&vcpu).unwrap_or_default(),
                     None => panic!("vcpu_affinity needs to be set for VirtCpufreq"),
                 };
-                vcpu_affinities.push(vcpu_affinity[0].try_into().unwrap());
-            }
-            let largest_vcpu_affinity_idx = *vcpu_affinities.iter().max().unwrap() as usize;
-            for (vcpu, vcpu_affinity) in vcpu_affinities.iter().enumerate() {
+
                 let mut virtfreq_size = AARCH64_VIRTFREQ_SIZE;
                 if components.virt_cpufreq_v2 {
                     let domain = *components.vcpu_domains.get(&vcpu).unwrap_or(&(vcpu as u32));
                     virtfreq_size = AARCH64_VIRTFREQ_V2_SIZE;
                     let virt_cpufreq = Arc::new(Mutex::new(VirtCpufreqV2::new(
-                        *vcpu_affinity,
+                        vcpu_affinity[0].try_into().unwrap(),
                         components.cpu_frequencies.clone(),
                         components.vcpu_domain_paths.get(&vcpu).cloned(),
                         domain,
                         *components.normalized_cpu_capacities.get(&vcpu).unwrap(),
-                        largest_vcpu_affinity_idx,
+                        vcpu_count,
                         vcpufreq_shared_tube.clone(),
                         freq_domain_vcpus.get(&domain).unwrap().clone(),
                     )));
@@ -766,7 +764,7 @@ impl arch::LinuxArch for AArch64 {
                         .map_err(Error::RegisterVirtCpufreq)?;
                 } else {
                     let virt_cpufreq = Arc::new(Mutex::new(VirtCpufreq::new(
-                        *vcpu_affinity,
+                        vcpu_affinity[0].try_into().unwrap(),
                         *components.normalized_cpu_capacities.get(&vcpu).unwrap(),
                         *components
                             .cpu_frequencies
--- devices/src/virtcpufreq_v2.rs
+++ devices/src/virtcpufreq_v2.rs
@@ -71,13 +71,7 @@ pub struct VirtCpufreqV2 {
     vm_ctrl: Arc<Mutex<Tube>>,
     pcpu_min_cap: u32,
     requested_util: u32,
-    /// The largest(or the last) pCPU index to be used by all the vCPUs. This index is used to
-    /// figure out the proper placement of the throttle workers which are placed on pCPUs right
-    /// after the last pCPU being used the vCPUs. Throttle workers require their own exclusive
-    /// pCPU allocation and this ensure that the workers are placed contiguously and makes it
-    /// easier for user to manage pCPU allocations when running multiple instances on a large
-    /// server.
-    largest_pcpu_idx: usize,
+    vcpu_count: usize,
     shared_domain_vcpus: Vec<usize>,
 }
 
@@ -125,7 +119,7 @@ impl VirtCpufreqV2 {
         vcpu_domain_path: Option<PathBuf>,
         vcpu_domain: u32,
         vcpu_capacity: u32,
-        largest_pcpu_idx: usize,
+        vcpu_count: usize,
         vm_ctrl: Arc<Mutex<Tube>>,
         shared_domain_vcpus: Vec<usize>,
     ) -> Self {
@@ -186,7 +180,7 @@ impl VirtCpufreqV2 {
             vm_ctrl,
             pcpu_min_cap,
             requested_util: 0,
-            largest_pcpu_idx,
+            vcpu_count,
             shared_domain_vcpus,
         }
     }
@@ -303,7 +297,7 @@ impl BusDevice for VirtCpufreqV2 {
                 if self.worker.is_none() {
                     let vcpu_id = info.id;
                     let vm_ctrl = self.vm_ctrl.clone();
-                    let worker_cpu_affinity = self.largest_pcpu_idx + self.vcpu_domain as usize + 1;
+                    let worker_cpu_affinity = self.pcpu as usize + self.vcpu_count;
                     let shared_domain_vcpus = self.shared_domain_vcpus.clone();
 
                     self.worker = Some(WorkerThread::start(
