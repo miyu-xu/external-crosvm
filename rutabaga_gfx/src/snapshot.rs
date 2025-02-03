bc898f5835eb6cd9dcea51cd2a3387c0cb69eeac
Jason Macnak <natsu@google.com>
2025-02-03 15:02:42 -0800

    Update snapshot helpers
    Bug: todo
    Test: todo
    Change-Id: I7cb41bc8b2b45e74e12d079b1c3b95e863ba4e6c

--- rutabaga_gfx/src/snapshot.rs
+++ rutabaga_gfx/src/snapshot.rs
@@ -2,92 +2,107 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO: remove in next change.
-#![allow(dead_code)]
-
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufWriter;
 use std::io::Write;
+use std::path::Path;
 use std::path::PathBuf;
 
-use crate::RutabagaError;
-use crate::RutabagaResult;
+use crate::rutabaga_utils::*;
 
-pub struct RutabagaSnapshotWriter {
-    dir: PathBuf,
-}
-
-impl RutabagaSnapshotWriter {
-    pub fn from_existing(directory: PathBuf) -> Self {
-        Self { dir: directory }
-    }
-
-    pub fn get_path(&self) -> PathBuf {
-        self.dir.clone()
-    }
-
-    pub fn add_namespace(&self, name: &str) -> RutabagaResult<Self> {
-        let directory = self.dir.join(name);
-
-        std::fs::create_dir(&directory).map_err(RutabagaError::IoError)?;
-
-        Ok(Self::from_existing(directory))
-    }
-
-    pub fn add_fragment<T: serde::Serialize>(&self, name: &str, t: &T) -> RutabagaResult<()> {
-        let fragment_path = self.dir.join(name);
-        let fragment_file = File::options()
-            .write(true)
-            .create_new(true)
-            .open(fragment_path)
-            .map_err(|e| {
-                RutabagaError::SnapshotError(format!("failed to add fragment {}: {}", name, e))
-            })?;
-        let mut fragment_writer = BufWriter::new(fragment_file);
-        serde_json::to_writer(&mut fragment_writer, t).map_err(|e| {
-            RutabagaError::SnapshotError(format!("failed to write fragment {}: {}", name, e))
-        })?;
-        fragment_writer.flush().map_err(|e| {
-            RutabagaError::SnapshotError(format!("failed to flush fragment {}: {}", name, e))
-        })?;
-        Ok(())
-    }
-}
-
-pub struct RutabagaSnapshotReader {
-    dir: PathBuf,
-}
-
-impl RutabagaSnapshotReader {
-    pub fn new(directory: PathBuf) -> RutabagaResult<Self> {
-        if !directory.as_path().exists() {
-            return Err(RutabagaError::SnapshotError(format!(
-                "{} does not exist",
-                directory.display()
-            )));
+fn get_files_recursively(directory: &Path, paths: &mut Vec<PathBuf>) -> RutabagaResult<()> {
+    if directory.is_dir() {
+        for entry in std::fs::read_dir(directory)? {
+            let entry = entry?;
+            let entry_path = entry.path();
+            if entry_path.is_dir() {
+                get_files_recursively(&entry_path, paths)?;
+            } else {
+                paths.push(entry_path.to_path_buf().clone());
+            }
         }
-
-        Ok(Self { dir: directory })
     }
+    Ok(())
+}
 
-    pub fn get_path(&self) -> PathBuf {
-        self.dir.clone()
-    }
+fn get_files_under(directory: &Path) -> RutabagaResult<Vec<PathBuf>> {
+    let mut paths = Vec::new();
+    get_files_recursively(directory, &mut paths)?;
+    Ok(paths)
+}
 
-    pub fn get_namespace(&self, name: &str) -> RutabagaResult<Self> {
-        let directory = self.dir.join(name);
-        Self::new(directory)
-    }
-
-    pub fn get_fragment<T: serde::de::DeserializeOwned>(&self, name: &str) -> RutabagaResult<T> {
-        let fragment_path = self.dir.join(name);
-        let fragment_file = File::open(fragment_path).map_err(|e| {
-            RutabagaError::SnapshotError(format!("failed to get fragment {}: {}", name, e))
+fn pack_directory_to_snapshot(directory: PathBuf) -> RutabagaResult<ciborium::Value> {
+    let directory = directory.as_path();
+    let directory_files = get_files_under(directory)
+        .map_err(|e| {
+            RutabagaError::SnapshotError(
+                format!("failed to list snapshot files under {}: {}", directory.display(), e)
+            )
         })?;
-        let mut fragment_reader = BufReader::new(fragment_file);
-        serde_json::from_reader(&mut fragment_reader).map_err(|e| {
-            RutabagaError::SnapshotError(format!("failed to read fragment {}: {}", name, e))
-        })
+
+    let mut paths_with_contents = Vec::new();
+    for path in directory_files.into_iter() {
+        let contents: Vec<u8> = std::fs::read(&path)
+            .map_err(|e| {
+                RutabagaError::SnapshotError(
+                    format!("failed to read snapshot file {}: {}", path.display(), e)
+                )
+            })?;
+
+        let relative_path = path
+            .strip_prefix(directory)
+            .map_err(|e| {
+                RutabagaError::SnapshotError(
+                    format!("failed to strip {} from {}: {}", directory.display(), path.display(), e)
+                )
+            })?
+            .to_string_lossy()
+            .to_string();
+
+        paths_with_contents.push((ciborium::Value::Text(relative_path), ciborium::Value::Bytes(contents)))
     }
+
+    Ok(ciborium::Value::Map(paths_with_contents))
+}
+
+fn unpack_snapshot_to_directory(directory: PathBuf, snapshot: ciborium::Value) -> RutabagaResult<()> {
+    let paths_to_contents_map =  snapshot
+        .as_map()
+        .ok_or(RutabagaError::SnapshotError(format!("recevied non map object from snapshot?")))?;
+
+    for (path_cbor, contents_cbor) in paths_to_contents_map.into_iter() {
+        let path = path_cbor
+            .as_text()
+            .ok_or(RutabagaError::SnapshotError(format!("found non string path in snapshot")))?;
+
+        let path = directory.join(path);
+        let path_directory = path
+            .parent()
+            .ok_or_else(|| {
+                RutabagaError::SnapshotError(
+                    format!("failed to get parent directory for {}", path.display())
+                )
+            })?;
+
+        std::fs::create_dir_all(path_directory)
+            .map_err(|e| {
+                RutabagaError::SnapshotError(
+                    format!("failed to create directories for {}: {}", path.display(), e)
+                )
+            })?;
+
+        let contents = contents_cbor
+            .as_bytes()
+            .ok_or(RutabagaError::SnapshotError(format!("found non bytes contents in snapshot")))?;
+
+        std::fs::write(&path, contents)
+            .map_err(|e| {
+                RutabagaError::SnapshotError(
+                    format!("failed to unpack snapshot to {}: {}", path.display(), e)
+                )
+            })?;
+    }
+
+    Ok(())
 }
