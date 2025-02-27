744717b6a130ff1c99527d2f1f414a51b8b98d0d
Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>
2025-02-27 09:44:31 -0800

    crosvm: gunyah: Support launching QTVMs
    Qualcomm Trusted VM (QTVM) is a kind of protected VM which differentiates
    itself from from the traditional protected VMs that have been supported by
    crosvm in a few aspects like -
    1. identified by reserved VM ID and PAS ID pair.
    2. authenticated by the firmware/TZ and not pvmfw.
    3. A QTVM Image is single loadable elf Image which contains kernel, dtb, initrd and the metadata
       needed for its authentication.
    4. A QTVM can be backed by Contiguous Memory. Either the entire GuestMemory region could be
       contiguous or it could be a combination of both CMA and shm.
    
    As the guest memory now can contain regions backed by CMA, make the necessary IOCTLs to map these
    regions to the VM. Also, add a new IOCTL to set the authentication type to be
    GUNYAH_QCOM_TRUSTED_VM_TYPE.
    
    Bug: 399219478
    Change-Id: I2e8a8f64db18602769421b667e69ca3a77c29426

--- src/crosvm/sys/linux/config.rs
+++ src/crosvm/sys/linux/config.rs
@@ -34,6 +34,8 @@ pub enum HypervisorKind {
     #[cfg(all(any(target_arch = "arm", target_arch = "aarch64"), feature = "gunyah"))]
     Gunyah {
         device: Option<PathBuf>,
+        qcom_trusted_vm_id: Option<u16>,
+        qcom_trusted_vm_pas_id: Option<u32>,
     },
 }
 
@@ -617,7 +619,11 @@ mod tests {
 
         assert_eq!(
             config.hypervisor,
-            Some(HypervisorKind::Gunyah { device: None })
+            Some(HypervisorKind::Gunyah {
+                device: None,
+                qcom_trusted_vm_id: None,
+                qcom_trusted_vm_pas_id: None,
+            })
         );
     }
 
@@ -635,7 +641,9 @@ mod tests {
         assert_eq!(
             config.hypervisor,
             Some(HypervisorKind::Gunyah {
-                device: Some(PathBuf::from("/not/default"))
+                device: Some(PathBuf::from("/not/default")),
+                qcom_trusted_vm_id: Some(0),
+                qcom_trusted_vm_pas_id: Some(0),
             })
         );
     }
