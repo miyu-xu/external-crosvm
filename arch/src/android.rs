ac2f16a08e51f92729f6e23274c35e40c1039bbf
miyu-xu <376807057@qq.com>
2026-06-27 20:48:05 +0800

    android: NUL-terminate fdt string properties

--- arch/src/android.rs
+++ arch/src/android.rs
@@ -18,6 +18,12 @@ fn parse_fstab_line(line: &str) -> Result<Vec<String>> {
     Ok(vec.iter().map(|s| s.to_string()).collect())
 }
 
+fn android_dt_string(value: &str) -> Vec<u8> {
+    let mut bytes = value.as_bytes().to_vec();
+    bytes.push(0);
+    bytes
+}
+
 /// Creates a flattened device tree containing all of the parameters used
 /// by Android.
 ///
@@ -32,24 +38,27 @@ pub fn create_android_fdt(fdt: &mut Fdt, fstab: File) -> Result<()> {
         .collect::<Result<Vec<Vec<String>>>>()?;
     let firmware_node = fdt.root_mut().subnode_mut("firmware")?;
     let android_node = firmware_node.subnode_mut("android")?;
-    android_node.set_prop("compatible", "android,firmware")?;
+    android_node.set_prop("compatible", android_dt_string("android,firmware"))?;
 
     let (dtprop, fstab): (_, Vec<_>) = vecs.into_iter().partition(|x| x[0] == "#dt-vendor");
     let vendor_node = android_node.subnode_mut("vendor")?;
     for vec in dtprop {
         let content = std::fs::read_to_string(&vec[2])?;
-        vendor_node.set_prop(&vec[1], content)?;
+        vendor_node.set_prop(&vec[1], android_dt_string(&content))?;
     }
     let fstab_node = android_node.subnode_mut("fstab")?;
-    fstab_node.set_prop("compatible", "android,fstab")?;
+    fstab_node.set_prop("compatible", android_dt_string("android,fstab"))?;
     for vec in fstab {
         let partition = &vec[1][1..];
         let partition_node = fstab_node.subnode_mut(partition)?;
-        partition_node.set_prop("compatible", "android,".to_owned() + partition)?;
-        partition_node.set_prop("dev", vec[0].as_str())?;
-        partition_node.set_prop("type", vec[2].as_str())?;
-        partition_node.set_prop("mnt_flags", vec[3].as_str())?;
-        partition_node.set_prop("fsmgr_flags", vec[4].as_str())?;
+        partition_node.set_prop(
+            "compatible",
+            android_dt_string(&("android,".to_owned() + partition)),
+        )?;
+        partition_node.set_prop("dev", android_dt_string(vec[0].as_str()))?;
+        partition_node.set_prop("type", android_dt_string(vec[2].as_str()))?;
+        partition_node.set_prop("mnt_flags", android_dt_string(vec[3].as_str()))?;
+        partition_node.set_prop("fsmgr_flags", android_dt_string(vec[4].as_str()))?;
     }
     Ok(())
 }
